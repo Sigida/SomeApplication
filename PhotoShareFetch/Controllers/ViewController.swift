55b4c1dbd3dd64d572c0f5798aaca9109b675e72
Roshy <Roshy_s7@icloud.com>
2018-07-11 22:56:33 +0300

    Start project
    Adding Pods, FireBase, Creating a project organization.

---
+++ PhotoShareFetch/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoShareFetch
+//
+//  Created by Admin on 11.07.2018.
+//  Copyright © 2018 roshy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
