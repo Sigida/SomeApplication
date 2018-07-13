607a9d9b9b29948c4c2f00d3127f1e0324e717e0
Roshy <Roshy_s7@icloud.com>
2018-07-13 11:02:55 +0300

    Creating Model

---
+++ PhotoShareFetch/Models/User.swift
@@ -0,0 +1,36 @@
+//
+//  User.swift
+//  PhotoShareFetch
+//
+//  Created by Admin on 12.07.2018.
+//  Copyright © 2018 roshy. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import FirebaseDatabase.FIRDataSnapshot
+
+class User {
+    
+    // MARK: - Properties
+    
+    let uid: String
+    let username: String
+    
+    // MARK: - Init
+    
+    init(uid: String, username: String) {
+        self.uid = uid
+        self.username = username
+    }
+    
+   //failable initializer
+    init?(snapshot: DataSnapshot) {
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+        
+        self.uid = snapshot.key
+        self.username = username
+    }
+}
