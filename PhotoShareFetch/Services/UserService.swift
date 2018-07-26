c9179a0d65b48981fc9f755f8af5ac16cf86f29d
Roshy <Roshy_s7@icloud.com>
2018-07-26 11:43:08 +0300

    Reading data from Firebase

--- PhotoShareFetch/Services/UserService.swift
+++ PhotoShareFetch/Services/UserService.swift
@@ -24,13 +24,11 @@ struct UserService {
             guard let user = User(snapshot: snapshot) else {
                 return completion(nil)
             }
-            
             completion(user)
         })
     }
     
     static func create(_ firUser: FIRUser, username: String, completion: @escaping (User?) -> Void) {
-        
         
          // create a dictionary to store the username the user has provided inside the database
         let userAttrs = ["username": username]
@@ -51,4 +49,18 @@ struct UserService {
             })
         }
 }
+    //retrieve all of a user's posts from Firebase
+    static func posts(for user: User, completion: @escaping ([Post]) -> Void) {
+        let ref = Database.database().reference().child("posts").child(user.uid)
+        
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let snapshot = snapshot.children.allObjects as? [DataSnapshot] else {
+                return completion([])
+            }
+            
+            let posts = snapshot.reversed().compactMap(Post.init)
+            completion(posts)
+        })
+    }
+    
 }
