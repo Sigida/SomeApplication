d0abe4e0e0a6de211022c7b8e0fc19fbda094d83
Roshy <Roshy_s7@icloud.com>
2018-08-02 13:26:29 +0300

    Setting-Folowers (Setting Outlets)

--- PhotoShareFetch/Controllers/FindFriendsViewController.swift
+++ PhotoShareFetch/Controllers/FindFriendsViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class FindFriendsViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- PhotoShareFetch/View/FindFriendsCell.swift
+++ PhotoShareFetch/View/FindFriendsCell.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class FindFriendsCell: UITableViewCell {
-
+   
+    // MARK: - Properties
+    
+    @IBOutlet weak var followButton: UIButton!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    // MARK: - Cell Lifecycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
- 
+    // MARK: - IBActions
+    
+    @IBAction func followButtonTapped(_ sender: UIButton) {
+        print("follow button tapped")
+    }
+
 }
