3c852e8eb71a6430ca76592b8da31a7c052aabf9
Roshy <Roshy_s7@icloud.com>
2018-08-02 12:52:40 +0300

    Setting-Folowers (Setting the UI)
    Creating FindFriends.storyboard, adding the TableView with constraints. Creating a FindFriendsCell with UIlabel and UIButton. Setting constraints.

---
+++ PhotoShareFetch/View/FindFriendsCell.swift
@@ -0,0 +1,19 @@
+//
+//  FindFriendsCell.swift
+//  PhotoShareFetch
+//
+//  Created by Admin on 02.08.2018.
+//  Copyright © 2018 roshy. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+ 
+}
